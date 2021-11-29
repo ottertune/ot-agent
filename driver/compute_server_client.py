1827125d2a414dfbb85408ac342ee2f542d6ef21
bohanjason <bohanzhang95@gmail.com>
2021-11-29 12:29:49 -0800

    bump version to 0.2.0

--- driver/compute_server_client.py
+++ driver/compute_server_client.py
@@ -16,7 +16,7 @@ RETRYABLE_HTTP_STATUS: Set[int] = {
 }
 
 # TODO: move this elsewhere and have it pull from git tags as source of truth
-AGENT_VERSION = "0.2"
+AGENT_VERSION = "0.2.0"
 
 
 class Observation(TypedDict):
