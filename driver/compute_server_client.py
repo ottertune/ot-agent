b01675e368978b3f2494f77b505a914504e7e11b
Yuze Liao <yuzeliao@ottertune.com>
2022-02-23 13:59:50 -0500

    Update version

--- driver/compute_server_client.py
+++ driver/compute_server_client.py
@@ -16,7 +16,7 @@ RETRYABLE_HTTP_STATUS: Set[int] = {
 }
 
 # TODO: move this elsewhere and have it pull from git tags as source of truth
-AGENT_VERSION = "0.3.1"
+AGENT_VERSION = "0.3.2"
 
 
 class Observation(TypedDict):
