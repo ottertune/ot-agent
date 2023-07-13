3f46ce86d9ce4aa291ce01fddb102c93e760f92c
Yuze Liao <yuzeliao@ottertune.com>
2023-07-13 16:54:11 -0400

    version number

--- driver/compute_server_client.py
+++ driver/compute_server_client.py
@@ -20,7 +20,7 @@ RETRYABLE_HTTP_STATUS: Set[int] = {
 }
 
 # TODO: move this elsewhere and have it pull from git tags as source of truth
-AGENT_VERSION = "0.4.6"
+AGENT_VERSION = "0.4.8"
 
 
 class DBLevelObservation(TypedDict):
