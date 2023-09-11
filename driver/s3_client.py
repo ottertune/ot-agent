f2d1e485fc63da16f62e8bf8d240370688b39381
Dao Chen <dao@ottertune.com>
2023-09-11 02:45:37 -0400

    Fix lint

--- driver/s3_client.py
+++ driver/s3_client.py
@@ -2,10 +2,11 @@
 
 import json
 from enum import Enum
-import boto3
 import datetime
 import zlib
+import boto3
 
+# pylint: disable=attribute-defined-outside-init
 
 class ObservationType(Enum):
     """Possible observation types."""
@@ -30,7 +31,8 @@ class S3Client:
         self._enable_s3 = enable_s3
         self._organization_id = organization_id
 
-    def _get_s3_session(self):
+    @staticmethod
+    def get_s3_session():
         """Get the S3 session."""
 
         session = boto3.Session()
@@ -47,53 +49,55 @@ class S3Client:
 
         return session
 
-    def _get_s3_bucket_object_key(self, type: ObservationType):
+    @staticmethod
+    def process_observation_data(data, obs_type: ObservationType):
+        """Process the observation data before posting to S3."""
+
+        if obs_type.value in (
+            ObservationType.QUERY.value,
+            ObservationType.LONG_RUNNING_QUERY.value,
+            ObservationType.SCHEMA.value,
+        ):
+            compressed_data = zlib.compress(
+                json.dumps(data, default=str).encode("utf-8")
+            )
+            return compressed_data
+
+        serialized_data = json.dumps(data).encode("utf-8")
+        return serialized_data
+
+    def get_s3_bucket_object_key(self, obs_type: ObservationType):
         """Get the S3 bucket object key for the observation data."""
 
         current_time = datetime.datetime.now(datetime.timezone.utc)
         timestamp_folder = current_time.strftime("%Y%m%d/%H")
         object_key = timestamp_folder + "/" + "data"
-        if type.value == ObservationType.DB.value:
+        if obs_type.value == ObservationType.DB.value:
             object_key = "DB/" + object_key
-        elif type.value == ObservationType.TABLE.value:
+        elif obs_type.value == ObservationType.TABLE.value:
             object_key = "TABLE/" + object_key
-        elif type.value == ObservationType.SCHEMA.value:
+        elif obs_type.value == ObservationType.SCHEMA.value:
             object_key = "SCHEMA/" + object_key
-        elif type.value == ObservationType.LONG_RUNNING_QUERY.value:
+        elif obs_type.value == ObservationType.LONG_RUNNING_QUERY.value:
             object_key = "LONG_RUNNING_QUERY/" + object_key
-        elif type.value == ObservationType.QUERY.value:
+        elif obs_type.value == ObservationType.QUERY.value:
             object_key = "QUERY/" + object_key
         else:
             object_key = "UNKNOWN/" + object_key
 
         return self._organization_id + "/" + object_key
 
-    def _process_observation_data(self, data, type: ObservationType):
-        """Process the observation data before posting to S3."""
 
-        if (
-            type.value == ObservationType.QUERY.value
-            or type.value == ObservationType.LONG_RUNNING_QUERY.value
-            or type.value == ObservationType.SCHEMA.value
-        ):
-            compressed_data = zlib.compress(
-                json.dumps(data, default=str).encode("utf-8")
-            )
-            return compressed_data
-        else:
-            serialized_data = json.dumps(data).encode("utf-8")
-            return serialized_data
-
-    def post_observation(self, data, type: ObservationType) -> None:
+    def post_observation(self, data, obs_type: ObservationType) -> None:
         """Post the observation to S3."""
 
         if not self._enable_s3:
             return
 
-        object_key = self._get_s3_bucket_object_key(type)
-        self._s3_session = self._get_s3_session()
-        self._s3_client = self._s3_session.client("s3")
-        processed_data = self._process_observation_data(data, type)
-        self._s3_client.put_object(
+        object_key = self.get_s3_bucket_object_key(obs_type)
+        s3_session = self.get_s3_session()
+        s3_client = s3_session.client("s3")
+        processed_data = self.process_observation_data(data, obs_type)
+        s3_client.put_object(
             Bucket=BUCKET_NAME, Key=object_key, Body=processed_data
         )
--- tests/s3_client_test.py
+++ tests/s3_client_test.py
@@ -1,18 +1,9 @@
 """
-Tests for the compute server client
+Tests for the S3 client
 """
-from typing import Dict, Any
-import pytest
-import responses
-import requests
-
-from driver.exceptions import ComputeServerClientException
-from unittest.mock import Mock, patch
-
-# Code under test
-from driver.s3_client import S3Client, ObservationType
 import zlib
 import json
+from driver.s3_client import S3Client, ObservationType
 
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
@@ -23,7 +14,7 @@ def test_get_s3_bucket_object_key() -> None:
         enable_s3=True,
         organization_id="81072a87-ef79-4a24-86be-84b2efd84688",
     )
-    object_key = client._get_s3_bucket_object_key(ObservationType.DB)
+    object_key = client.get_s3_bucket_object_key(ObservationType.DB)
     assert "81072a87-ef79-4a24-86be-84b2efd84688/DB/" in object_key
     assert "data" in object_key
 
@@ -36,34 +27,34 @@ def test_process_observation_data() -> None:
     observation_data = {
         "key1": "value1",
     }
-    processed_observation_data = client._process_observation_data(
+    processed_observation_data = client.process_observation_data(
         observation_data, ObservationType.QUERY
     )
     decompressed_data = zlib.decompress(processed_observation_data).decode("utf-8")
     assert isinstance(processed_observation_data, bytes), True
     assert decompressed_data, json.dumps(observation_data, default=str)
 
-    processed_observation_data = client._process_observation_data(
+    processed_observation_data = client.process_observation_data(
         observation_data, ObservationType.SCHEMA
     )
     decompressed_data = zlib.decompress(processed_observation_data).decode("utf-8")
     assert isinstance(processed_observation_data, bytes), True
     assert decompressed_data, json.dumps(observation_data, default=str)
 
-    processed_observation_data = client._process_observation_data(
+    processed_observation_data = client.process_observation_data(
         observation_data, ObservationType.LONG_RUNNING_QUERY
     )
     decompressed_data = zlib.decompress(processed_observation_data).decode("utf-8")
     assert isinstance(processed_observation_data, bytes), True
     assert decompressed_data, json.dumps(observation_data, default=str)
 
-    processed_observation_data = client._process_observation_data(
+    processed_observation_data = client.process_observation_data(
         observation_data, ObservationType.DB
     )
     assert isinstance(processed_observation_data, bytes), True
     assert processed_observation_data, json.dumps(observation_data)
 
-    processed_observation_data = client._process_observation_data(
+    processed_observation_data = client.process_observation_data(
         observation_data, ObservationType.TABLE
     )
     assert isinstance(processed_observation_data, bytes), True
